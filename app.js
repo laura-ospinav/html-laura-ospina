09298cefdfc7a08b40ad8880569f43ed9408e092
laura-ospinav <123768080+laura-ospinav@users.noreply.github.com>
2023-02-10 14:00:18 -0500

    clase enero 10

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {});
+const cardAdj = [
+  {
+    name: "a",
+    img: "images/a.png"
+  }
+];
+[
+  {
+    name: "b",
+    img: "images/b.png"
+  }
+];
+[
+  {
+    name: "c",
+    img: "images/c.png"
+  }
+];
+[
+  {
+    name: "d",
+    img: "images/d.png"
+  }
+];
+[
+  {
+    name: "e",
+    img: "images/e.png"
+  }
+];
+[
+  {
+    name: "f",
+    img: "images/f.png"
+  }
+];
+
+const cuadricula = document.querySelector(".cuadricula");
+const resultado = document.querySelector("#resultado");
+var cartasEscogidas = [];
+var cartasEscogidasId = [];
+var cartasGanadas = [];
